ebd3ba320c73dddc5d701be8d5fe9d502597d840
Madasanya <margarete.hanna.mueller@gmail.com>
2025-06-08 21:42:58 +0200

    generic blink function with delay and pause option

--- 00/blinkingLED/blinkingLED.ino
+++ 00/blinkingLED/blinkingLED.ino
@@ -9,18 +9,58 @@ const int PIN_BUTTON = 3;
 const int MODES = 1;
 
 //variables
-int ledStateGreen = 1;
-int ledStateRed = 0;
-int ledStateYellow = 0;
+int ledStateGreen = HIGH;
+int ledStateRed = LOW;
+int ledStateYellow = LOW;
 int mode = 0;
 int buttonState;
-unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+unsigned long previousMillisRed = 0;
+unsigned long previousMillisYellow = 0;
 int lastButtonState = LOW;
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+int ledBlinkStateGreen = 0;
+int ledBlinkStateRed = 0;
+int ledBlinkStateYellow = 0;
+
+void blink (const int pin, unsigned long delay, unsigned long duration, unsigned long pause, int *ledState, unsigned long * previousMillis)
+{
+  switch (*ledState)
+  {
+    case 0:
+    	*previousMillis = currentMillis;
+    	digitalWrite(pin, LOW);
+    	*ledState = 1; 
+    	break;
+    case 1:
+    	if (currentMillis - *previousMillis > delay) 
+        {
+          digitalWrite(pin, HIGH);
+          *ledState = 2;
+          *previousMillis = currentMillis;
+        }
+    	break;
+    case 2:
+    	if (currentMillis - *previousMillis > duration) 
+        {
+          digitalWrite(pin, LOW);
+          *ledState = 3;
+          *previousMillis = currentMillis;
+        }
+    	break;
+    default:
+    	if (currentMillis - *previousMillis > pause) 
+        {
+          digitalWrite(PIN_LED_RED, LOW);
+          *ledState = 0;
+          *previousMillis = currentMillis;
+        } 	
+  }
+}
 
 // if the LED is off turn it on and vice-versa:
-int revLEDState(int *ledState)
+int reverseLEDState(int *ledState)
 {
   if (*ledState == LOW) {
     *ledState = HIGH;
@@ -43,7 +83,7 @@ void setup()
 
 void loop()
 {
-  unsigned long currentMillis = millis();
+  currentMillis = millis();
   int reading = digitalRead(PIN_BUTTON);
   // If the switch changed, due to noise or pressing:
   if (reading != lastButtonState) {
@@ -62,7 +102,8 @@ void loop()
           mode = 0;
         else
           mode = (mode % MODES) + 1;
-        previousMillis = 0;
+        previousMillisRed = currentMillis;
+        previousMillisYellow = currentMillis;
       }
     }
   }
@@ -72,13 +113,16 @@ void loop()
     case 1:
       ledStateGreen = LOW;
       digitalWrite(PIN_LED_GREEN, ledStateGreen);
-      if (currentMillis - previousMillis >= 500) 
-      {
-        // save the last time you blinked the LED
-        previousMillis = currentMillis;
-        digitalWrite(PIN_LED_RED, revLEDState(&ledStateRed));
-        digitalWrite(PIN_LED_YELLOW, revLEDState(&ledStateYellow));
-      }
+      blink(PIN_LED_RED, 1000, 100, 900, &ledBlinkStateRed, &previousMillisRed);
+      blink(PIN_LED_YELLOW, 0, 100, 1900, &ledBlinkStateYellow, &previousMillisYellow);
+      break;
+      // if (currentMillis - previousMillis >= 500) 
+      // {
+      //   // save the last time you blinked the LED
+      //   previousMillis = currentMillis;
+      //   digitalWrite(PIN_LED_RED, reverseLEDState(&ledStateRed));
+      //   digitalWrite(PIN_LED_YELLOW, reverseLEDState(&ledStateYellow));
+      // }
       break;
     //case 2:
     //  digitalWrite(PIN_LED_GREEN, revLEDState(&ledStateGreen));

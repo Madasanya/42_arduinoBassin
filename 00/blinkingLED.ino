8b18a167b8e5f13a055feca46e2696b02502fefe
Madasanya <margarete.hanna.mueller@gmail.com>
2025-06-08 15:21:56 +0200

    button status control and debouncing added

--- 00/blinkingLED.ino
+++ 00/blinkingLED.ino
@@ -6,14 +6,18 @@ const int PIN_LED_GREEN = 13;
 const int PIN_LED_RED = 12;
 const int PIN_LED_YELLOW = 11;
 const int PIN_BUTTON = 3;
-const int CASES =1;
+const int MODES = 1;
 
 //variables
-int buttonState = 0;
-int ledStateGreen = HIGH;
-int ledStateRed = LOW;
-int ledStateYellow = LOW;
+int ledStateGreen = 1;
+int ledStateRed = 0;
+int ledStateYellow = 0;
+int mode = 0;
+int buttonState;
 unsigned long previousMillis = 0;
+int lastButtonState = LOW;
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 // if the LED is off turn it on and vice-versa:
 int revLEDState(int *ledState)
@@ -32,6 +36,7 @@ void setup()
   pinMode(PIN_LED_RED, OUTPUT);
   pinMode(PIN_LED_YELLOW, OUTPUT);
   pinMode(PIN_BUTTON, INPUT_PULLUP);
+  mode = 0;
   digitalWrite(PIN_LED_GREEN, ledStateGreen);
   delay(500);
 }
@@ -39,15 +44,30 @@ void setup()
 void loop()
 {
   unsigned long currentMillis = millis();
-  if (digitalRead(PIN_BUTTON) == 0) 
-  {
-    if (buttonState == CASES)
-      buttonState = 0;
-    else
-      buttonState = (buttonState % CASES) + 1;
-    previousMillis = 0;
+  int reading = digitalRead(PIN_BUTTON);
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
   }
-  switch (buttonState)
+  if ((millis() - lastDebounceTime) > debounceDelay) 
+  {
+    if (reading != buttonState) 
+    {
+      buttonState = reading;
+
+      if (buttonState == LOW)
+      {
+        if (mode == MODES)
+          mode = 0;
+        else
+          mode = (mode % MODES) + 1;
+        previousMillis = 0;
+      }
+    }
+  }
+  
+  switch (mode)
   {
     case 1:
       ledStateGreen = LOW;
@@ -79,4 +99,6 @@ void loop()
       digitalWrite(PIN_LED_RED, LOW);
       digitalWrite(PIN_LED_YELLOW, LOW); 
   }
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
 }
